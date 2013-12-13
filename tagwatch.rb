02c259e4e11aa397e69ace6e61f4125309bee745
Chris Toomey <chris@ctoomey.com>
2013-12-13 10:46:01 -0500

    Add option to install watchdog via PIP

--- tagwatch.rb
+++ tagwatch.rb
@@ -5,7 +5,15 @@ class Tagwatch < Formula
   url 'https://github.com/djcp/tagwatch/archive/v0.0.1.zip'
   sha1 '673b4769f93271a8ebae60ff83630e240f908b3f'
 
+  option 'with-watchdog', 'Install watchdog via pip'
+
+  depends_on :python if build.include? 'with-watchdog'
+
   def install
+    if build.include? 'with-watchdog'
+      system 'pip install watchdog'
+    end
+
     bin.install 'tagwatch.sh' => 'tagwatch'
   end
 end
